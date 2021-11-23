0aa83f63e8d2625f7389c19abb6be28041f0f897
Punane <kadikerner@gmail.com>
2021-11-23 08:18:04 +0200

    main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './Header';
+import Footer from './Footer';
+import Main from './Main';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <h1>Mathificent</h1>
+      <Main />
       <Footer />
     </div>
   );
---
+++ src/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import SelectInput from "./SelectInput";
+import PlayButton from "./PlayButton";
+
+function Main() {
+    return (
+        <div>
+            <h1>Mathificent</h1>
+            <SelectInput />
+            <SelectInput />
+            <PlayButton />
+        </div>
+    )
+}
+
+export default Main;
